916a1aca9e39a3e17ebcc2a261fb93c417529eea
Nikita <manager@keenagency.ru>
2016-11-21 08:14:11 +0300

    refactor for better use of browserify

--- scripts.js
+++ scripts.js
@@ -1,21 +1,55 @@
 'use strict';
 
+var browserify = require('browserify'),
+  source       = require('vinyl-source-stream'),
+  buffer       = require('vinyl-buffer'),
+  sourcemaps   = require('gulp-sourcemaps'),
+  gutil        = require('gulp-util'),
+
+  //Get global config
+  config       = $.config.js,
+  vendors      = config.bundle.vendors;
+
 module.exports = function() {
 
-  //Processes js files: browserify, uglify and moves to destination folder
-  $.gulp.task('js:process', function() {
-    return $.gulp.src($.config.js.entryPoint)
+  //Build vendors to separate package
+  $.gulp.task('js:bundle', function() {
+    var b = browserify({
+      debug: !$.releaseFlag
+    });
+
+    vendors.forEach(function(lib) {
+      b.require(lib);
+    });
+
+    return b.bundle()
       .pipe($.plugins.plumber())
-      .pipe($.plugins.browserify($.config.js.config.process.browserify))
-      .pipe($.plugins.if($.argv.release,
-        $.plugins.uglify()
-      ))
-      .pipe($.plugins.rename($.config.js.config.process.result))
-      .pipe($.plugins.if($.argv.release === undefined,
-        $.gulp.dest($.config.js.destinationDev)
-      ))
-      .pipe($.plugins.if($.argv.release,
-        $.gulp.dest($.config.js.destinationRls)
-      ));
+      .pipe(source(config.bundle.result))
+      .pipe(buffer())
+      .pipe($.releaseFlag ? gutil.noop() : sourcemaps.init({loadMaps: true}))
+      .pipe($.releaseFlag ? $.plugins.uglify() : gutil.noop())
+      .pipe($.releaseFlag ? gutil.noop() : sourcemaps.write('./'))
+      .pipe($.releaseFlag ? $.gulp.dest(config.destinationRls) : $.gulp.dest(config.destinationDev));
+  });
+
+  //Build app
+  $.gulp.task('js:app', function() {
+    var b = browserify({
+      entries : config.entryPoint,
+      debug   : !$.releaseFlag
+    });
+
+    vendors.forEach(function(lib) {
+      b.external(lib);
+    });
+
+    return b.bundle()
+      .pipe($.plugins.plumber())
+      .pipe(source(config.app.result))
+      .pipe(buffer())
+      .pipe($.releaseFlag ? gutil.noop() : sourcemaps.init({loadMaps: true}))
+      .pipe($.releaseFlag ? $.plugins.uglify() : gutil.noop())
+      .pipe($.releaseFlag ? gutil.noop() : sourcemaps.write('./'))
+      .pipe($.releaseFlag ? $.gulp.dest(config.destinationRls) : $.gulp.dest(config.destinationDev));
   });
 };
