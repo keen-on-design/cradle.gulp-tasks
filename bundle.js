5def9d5ca849fca10291bd7bffedfb53238a1c09
Nikita <manager@keenagency.ru>
2017-01-09 04:35:55 +0300

    let instead of var

--- bundle.js
+++ bundle.js
@@ -1,43 +1,46 @@
 'use strict';
 
-var gulp       = require('gulp'),
-  gutil        = require('gulp-util'),
-
-  rename       = require('gulp-rename'),
-  browserify   = require('browserify'),
-  uglify       = require('gulp-uglify'),
-  source       = require('vinyl-source-stream'),
-  buffer       = require('vinyl-buffer'),
-  plumber      = require('gulp-plumber'),
-  sourcemaps   = require('gulp-sourcemaps'),
-  _            = require('underscore'),
-
-  // Default config
-  defaults     = {
-    entryPoint : './src/js/app.js',
-    destination : {
-      production  : './build/js',
-      development : './build/js'
-    },
-    browserify : { },
-    output : 'bundle.js'
-  };
-
 /**
  * Creates a bundler task
  * @param {String} [id=js:bundle] Id of a task
  * @param {Object} [config={}] Task config
- * @return {Stream}
  */
 module.exports = function (id, config) {
-  var wizard = require('./utils/c.wizard')(id, 'js:bundle', config, defaults),
-    vendors = _.keys($.package.dependencies);
+  let
+    // Default config
+    defaultId = 'js:bundle',
+    defaults = {
+      entryPoint: './src/js/app.js',
+      destination: {
+        production:  './build/js',
+        development: './build/js',
+      },
+      browserify: { },
+      sourcemaps: {loadMaps: true},
+      output: 'bundle.js',
+    },
+    // Init task with cradle wizard
+    wizard = require('./utils/c.wizard')(id, defaultId, config, defaults),
+    // Task dependencies
+    gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    rename = require('gulp-rename'),
+    browserify = require('browserify'),
+    uglify = require('gulp-uglify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    plumber = require('gulp-plumber'),
+    sourcemaps = require('gulp-sourcemaps'),
+    _ = require('underscore'),
+    vendors,
+    bundler;
+  vendors = _.keys($.package.dependencies);
 
   id     = wizard.getId();
   config = wizard.getConfig();
 
   gulp.task(id, function () {
-    var bundler = browserify(_.extend({
+    bundler = browserify(_.extend({
       debug: $.env.debug
     }, config.browserify));
 
@@ -49,9 +52,9 @@ module.exports = function (id, config) {
       .pipe(plumber())
       .pipe(source(config.output))
       .pipe(buffer())
-      .pipe($.env.production ? gutil.noop() : sourcemaps.init({loadMaps: true}))
+      .pipe($.env.production ? gutil.noop() : sourcemaps.init(config.sourcemaps))
       .pipe($.env.production ? uglify() : gutil.noop())
-      .pipe($.env.production ? rename({suffix : '.min'}) : gutil.noop())
+      .pipe($.env.production ? rename({suffix: '.min'}) : gutil.noop())
       .pipe($.env.production ? gutil.noop() : sourcemaps.write('./'))
       .pipe($.env.production ? gulp.dest(config.destination.production) : gulp.dest(config.destination.development));
   });
