67cfe101fe8306a2dc798986865532612628063d
Nikita <maccuro.media@gmail.com>
2017-01-10 20:37:17 +0300

    linted

--- relocate.js
+++ relocate.js
@@ -1,29 +1,31 @@
 'use strict';
-var gulp = require('gulp'),
-  wizard = require('./utils/c.wizard'),
-
-  // Default config
-  defaults     = {
-    location : './src/dummy/**/*.',
-    destination : {
-      production  : './build/dummy',
-      development : './build/dummy'
-    }
-  };
 
 /**
  * Creates a relocate task. Moves files from one location to another
  * @param {String} [id=clean] Id of a task
  * @param {Object} [config={}] Task config
- * @return {Function}
  */
 module.exports = function (id, config) {
-  var wizard = require('./utils/c.wizard')(id, 'relocate', config, defaults);
+  // Default config
+  let defaultId = 'relocate';
+  let defaults = {
+    location: './src/dummy/**/*.',
+    destination: {
+      production: './build/dummy',
+      development: './build/dummy'
+    }
+  };
 
-  id     = wizard.getId();
+  // Init task with cradle wizard
+  let wizard = require('./utils/c.wizard')(id, defaultId, config, defaults);
+
+  // Task dependencies
+  let gulp = require('gulp');
+
+  // Final task config
   config = wizard.getConfig();
 
-  gulp.task(id, function () {
+  gulp.task(wizard.getId(), function () {
     return gulp.src(config.location)
       .pipe($.env.production ? gulp.dest(config.destination.production) : gulp.dest(config.destination.development));
   });
