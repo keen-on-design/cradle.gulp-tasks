ac843c42feb129233580441212252c73198b046c
Nikita <manager@keenagency.ru>
2017-01-11 13:05:47 +0300

    utilize c.wizard

--- serve.js
+++ serve.js
@@ -1,12 +1,32 @@
 'use strict';
 
-module.exports = function () {
-  $.gulp.task('serve', function () {
-    $.browserSync.init({
+/**
+ * Browsersync server task.
+ * @param {String} [id=clean] Id of a task
+ * @param {Object} [config={}] Task config
+ */
+module.exports = function (id, config) {
+  // Default config
+  let defaultId = 'serve';
+  let defaults = {
+    server: {
       open: false,
-      server: $.config.destDev
-    });
+      server: './build/'
+    },
+    watch: ['./build/**/*.*', '!**/*.css']
+  };
 
-    $.browserSync.watch([$.config.destDev + '**/*.*', '!**/*.css'], $.browserSync.reload);
+  // Init task with cradle wizard
+  let wizard = require('./utils/c.wizard')(id, defaultId, config, defaults);
+
+  // Task dependencies
+  let gulp = require('gulp');
+
+  // Final task config
+  config = wizard.getConfig();
+
+  gulp.task(wizard.getId(), function () {
+    $.browserSync.init(config.server);
+    $.browserSync.watch(config.watch, $.browserSync.reload);
   });
 };
