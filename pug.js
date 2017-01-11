ff22ac0e97ae4d9759e3e138c3e0abcec7e76204
Nikita <manager@keenagency.ru>
2017-01-11 13:06:13 +0300

    linted

--- pug.js
+++ pug.js
@@ -1,24 +1,53 @@
 'use strict';
 
-module.exports = function () {
+/**
+ * Pug (Jade) task. Utilizes useref to replace references to non-optimized scripts or stylesheets
+ * @param {String} [id=clean] Id of a task
+ * @param {Object} [config={}] Task config
+ */
+module.exports = function (id, config) {
+  // Default config
+  let defaultId = 'pug';
+  let defaults = {
+    entryPoint: './src/template/_pages/*.pug',
+    destination: {
+      development: './build',
+      production: './build'
+    },
+    pug: {
+      pretty: '\t',
+      basedir: './'
+    },
+    useref: {}
+  };
 
-  //Compiles pug templates
-  $.gulp.task('pug:process', function () {
-    return $.gulp.src($.config.pug.compiled)
-      .pipe($.plugins.plumber())
-      .pipe($.plugins.pug($.config.pug.config))
-      .pipe($.plugins.useref())
-      .pipe($.plugins.if('*.js',
-        $.plugins.if($.argv.release, $.plugins.uglify())
+  // Init task with cradle wizard
+  let wizard = require('./utils/c.wizard')(id, defaultId, config, defaults);
+
+  // Task dependencies
+  let gulp = require('gulp');
+  let gutil = require('gulp-util');
+  let plumber = require('gulp-plumber');
+  let pug = require('gulp-pug');
+  let useref = require('gulp-useref');
+  let cssnano = require('gulp-cssnano');
+  let gulpIf = require('gulp-if');
+  let uglify = require('gulp-uglify');
+
+  // Final task config
+  config = wizard.getConfig();
+
+  gulp.task(wizard.getId(), function () {
+    return gulp.src(config.entryPoint)
+      .pipe(plumber())
+      .pipe(pug(config.pug))
+      .pipe(useref(config.useref))
+      .pipe(gulpIf('*.js',
+        $.env.production ? uglify() : gutil.noop()
       ))
-      .pipe($.plugins.if('*.css',
-        $.plugins.if($.argv.release, $.plugins.cssnano())
+      .pipe(gulpIf('*.css',
+        $.env.production ? cssnano() : gutil.noop()
       ))
-      .pipe($.plugins.if($.argv.release,
-        $.gulp.dest($.config.pug.destinationRls)
-      ))
-      .pipe($.plugins.if($.argv.release === undefined,
-        $.gulp.dest($.config.pug.destinationDev)
-      ));
+      .pipe($.env.production ? gulp.dest(config.destination.production) : gulp.dest(config.destination.development));
   });
 };
